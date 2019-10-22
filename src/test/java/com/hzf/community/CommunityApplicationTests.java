4845029da6312db90451dea59eff20ea73097ba2
hzf1997 <2832291250@qq.com>
2019-10-22 17:16:09 +0800

    first push

---
+++ src/test/java/com/hzf/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzf.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
